79a81a6ed43327098876b71dff2bd460801d4b4d
ooxiaoyan <810978527@qq.com>
2018-07-12 23:35:37 +0800

    feat(triangle_exercises): add Easiest_exercise_ever

---
+++ src/triangle_exercises/Easiest_exercise_ever.java
@@ -0,0 +1,15 @@
+package triangle_exercises;
+
+/**
+ * Created by 筱湮 on 2018/7/12 0012.
+ * Print one asterisk to the console.
+ * Example:
+ * *
+ *
+ */
+public class Easiest_exercise_ever {
+
+    public static void main(String[] args) {
+        System.out.println("*");
+    }
+}
